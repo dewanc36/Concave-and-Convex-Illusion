88d197a2aac073a8c2d4dfd10dd5e2e31232c803
Dewan Choudhury <dewan@dewans-macbook-pro.local>
2016-07-18 23:12:59 -0400

    Initial Commit

---
+++ Project1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project1
+//
+//  Created by Dewan Adil Choudhury on 7/18/16.
+//  Copyright © 2016 Dewan Adil Choudhury. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
